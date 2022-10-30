d0061494c7bb78335007c9c6fb00d64d6c8bd82c
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-10-30 17:49:55 +0300

    added Course validator messages

---
+++ Kodlama.io simule/src/core/validator/concretes/constants/CourseValidatorMessages.java
@@ -0,0 +1,6 @@
+package core.validator.concretes.constants;
+
+public class CourseValidatorMessages {
+    public static final String COURSE_NAME_MUST_BE_LEAST_ONE_CHARACTERS = "Kurs adı en az bir karakter olmalı.";
+    public static final String THE_PRICE_OF_THE_COURSE_CANNOT_BE_LESS_THAN_ZERO = "Kurs fiyatı 0'dan küçük olamaz.";
+}
